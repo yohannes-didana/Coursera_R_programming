7a2b1b345cd156089856071139a1d034255b2d85
yohannes-didana <yohanes.didana@gmail.com>
2020-04-29 03:58:56 +1000

    Add existing file

--- quiz1.R
+++ quiz1.R
@@ -66,3 +66,36 @@ p <- as.POSIXlt(x)
 p
 names(unclass(p))
 p$sec
+
+
+
+cube <- function(x, n) {
+  x^3
+}
+
+cube(3)
+
+x <- 1:10
+if(x > 5) {
+  x <- 0
+}
+
+f <- function(x) {
+  g <- function(y) {
+    y + z
+  }
+  z <- 4
+  x + g(x)
+}
+
+z <- 10
+f(3)
+
+
+
+
+
+
+
+
+
