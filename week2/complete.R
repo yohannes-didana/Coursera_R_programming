659305314d94fddc51b5a1b6a42ffe637a8a61a3
yohannes-didana <yohanes.didana@gmail.com>
2020-04-29 05:09:44 +1000

    week2

--- week2/complete.R
+++ week2/complete.R
@@ -18,17 +18,20 @@ complete <- function(directory, id = 1:332) {
   if(grep("specdata", directory) == 1) {
     directory <- ("./specdata/")
   }
+  # get the length of id vector
+  id_len <- length(id)
+  complete_data <- rep(0, id_len)
   # find all files in the specdata folder
   all_files <- as.character( list.files(directory) )
   file_paths <- paste(directory, all_files, sep="")
   j <- 1 
   for (i in id) {
-    file <- read.csv(file_paths[i], header=T, sep=",")
-    complete_data[j] <- sum(complete.cases(file))
+    current_file <- read.csv(file_paths[i], header=T, sep=",")
+    complete_data[j] <- sum(complete.cases(current_file))
     j <- j + 1
   }
   result <- data.frame(id = id, nobs = complete_data)
-  result
+  return(result)
   
 }
 
